f664eb6d27899288e3320fc5eec4b6da8015f936
Sharath <Sharus9587@gmail.com>
2021-05-07 17:37:18 +0530

    Added : Implentation of NLog Program

---
+++ NLogImplementation/NLogImplementation/AddNumbers.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NLogImplementation
+{
+    public class AddNumbers
+    {
+        NLog nlog = new NLog();
+        public int Sum(int a, int b)
+        {
+            if (a == 0 || b == 0)
+            {
+                nlog.LogDebug("Debug Successfull");
+                nlog.LogError("expecting Null value");
+                nlog.LogWarn("a or b value should not be successfull");
+            }
+            int c = a + b;
+            nlog.LogDebug("Debug Successfull");
+            return c;
+        }
+    }
+}
